a6a46166295c552b41c670c90812046051787059
Patryck <patryck.s.eloy@gmail.com>
2024-10-01 21:41:10 -0300

    Criação de tela de login

---
+++ app/src/main/java/br/com/victall/projetoph/activity/MainActivity.java
@@ -0,0 +1,23 @@
+package br.com.victall.projetoph.activity;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.util.Log;
+
+import br.com.victall.projetoph.R;
+import br.com.victall.projetoph.model.Livro;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+
+    }
+
+
+
+    }
---
+++ app/src/main/java/br/com/victall/projetoph/model/Livro.java
@@ -0,0 +1,45 @@
+package br.com.victall.projetoph.model;
+
+public class Livro {
+    String titulo;
+    String autor;
+    int anoPublicacao;
+    double preco;
+
+    public String getTitulo() {
+        return titulo;
+    }
+
+    public void setTitulo(String titulo) {
+        this.titulo = titulo;
+    }
+
+    public String getAutor() {
+        return autor;
+    }
+
+    public void setAutor(String autor) {
+        this.autor = autor;
+    }
+
+    public int getAnoPublicacao() {
+        return anoPublicacao;
+    }
+
+    public void setAnoPublicacao(int anoPublicacao) {
+        this.anoPublicacao = anoPublicacao;
+    }
+
+    public double getPreco() {
+        return preco;
+    }
+
+    public void setPreco(double preco) {
+        this.preco = preco;
+    }
+
+    public void exibirInformacoes(){
+
+    }
+
+}
