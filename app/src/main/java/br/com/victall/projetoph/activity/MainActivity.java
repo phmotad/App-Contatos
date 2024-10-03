6428004d5fe706c42de46fe7d2f583620491e223
Patryck <patryck.s.eloy@gmail.com>
2024-10-03 20:34:29 -0300

    Criação de tela de login

--- app/src/main/java/br/com/victall/projetoph/activity/MainActivity.java
+++ app/src/main/java/br/com/victall/projetoph/activity/MainActivity.java
@@ -2,22 +2,56 @@ package br.com.victall.projetoph.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.icu.lang.UCharacterEnums;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.Button;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.Toast;
 
 import br.com.victall.projetoph.R;
 import br.com.victall.projetoph.model.Livro;
 
 public class MainActivity extends AppCompatActivity {
+    private EditText editEmail;
+    private EditText editNome;
+    private EditText editsenha;
+    private Button btnCadastro;
+    private CheckBox cbTermos;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        editNome = findViewById(R.id.editTextText);
+        editEmail = findViewById(R.id.editTextTextEmailAddress2);
+        editsenha = findViewById(R.id.editTextTextPassword2);
+        btnCadastro = findViewById(R.id.button);
+        cbTermos = findViewById(R.id.checkBox2);
 
     }
-
-
-
+    private boolean verificaCampos(){
+        if(!editNome.getText().toString().isEmpty())
+            if(!editEmail.getText().toString().isEmpty())
+                if(!editsenha.getText().toString().isEmpty())
+                    return true;
+        return false;
     }
+    private boolean verificaSenha(){
+        if(editsenha.getText().toString().length()<6)
+            return false;
+        return true;
+    }
+    private boolean verificarEmail(){
+        return editEmail.getText().toString().contains("@");
+    }
+    public void cadastrar(View view){
+        if (verificaCampos())
+            if(verificaSenha())
+                if(verificarEmail())
+                    Toast.makeText(this, "Cadastro efetuado com sucesso!", Toast.LENGTH_SHORT).show();
+    }
+}
